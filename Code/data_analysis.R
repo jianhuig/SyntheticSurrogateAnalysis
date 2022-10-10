1cf283b67866aa9aa8f6be3ddee0fb89f8527023
jianhuig <58570421+jianhuig@users.noreply.github.com>
2022-10-09 23:39:50 -0400

    clean up code

--- Code/data_analysis.R
+++ Code/data_analysis.R
@@ -4,79 +4,98 @@ library(bigsnpr)
 library(doParallel)
 library(BEDMatrix)
 
-create_missing_phenotypes <- function(field, in_id_file, rate, seed = 123) {
-  temp <- readRDS(paste0("field_", field, "_cleaned.rds")) # cleaned pheno + cov
-  in_id <- readRDS(in_id_file)
-  temp <- temp %>% filter(f.eid %in% in_id)
+library(ranger)
+library(readr)
+library(dplyr)
+library(data.table)
+library(tidyr)
+
+#setwd("~/Desktop/SyntheticSurrogateAnalysis/Data")
+
+cov_id <- c("f.21022.0.0","f.22001.0.0","f.50.0.0","f.23098.0.0","f.23104.0.0") #age,sex,height, weight, BMI
+#pheno_id <- c("f.23248.2.0")
+pheno_id <- (commandArgs(TRUE))
+print(pheno_id)
+
+# split data into train and test
+file <- read.table("CombinedTotalMass.tab", header=TRUE, sep="\t")
+in_id <- readRDS("in.id.rds")
+out_id <- readRDS("out.id.rds")
+
+cov_column <- file %>% select(cov_id)
+train <- file %>% filter(f.eid %in% out_id) %>% tidyr::drop_na(pheno_id) %>% tidyr::drop_na(all_of(cov_id)) %>% select(pheno_id, cov_id)
+test <- file %>% filter(f.eid %in% in_id) %>% tidyr::drop_na(all_of(cov_id))%>% select(f.eid,pheno_id, cov_id)
+
+# Random Forest model
+model.rf <- ranger::ranger(
+  data = train,
+  as.formula(paste0(pheno_id, "~."))
+)
+
+yhat <- predict(model.rf, data = test %>% select(-f.eid,-pheno_id))$predictions
+test$yhat <- yhat
+
+# Merge genetic PC
+eigenvec <- fread("plink2.eigenvec") %>% rename(f.eid = `#FID`) %>% select(-IID)
+test <- test %>% inner_join(eigenvec, by = "f.eid")
+
+# Inverse Normal Transformation
+INT <- function(data, pheno, k = 0.375){
+  missing_index <- which(is.na(data[[pheno]]))
+  data.complete <- data[-missing_index, ]
+  data.missing <-data[missing_index, ]
   
-  # number of missing phenotypes
-  nmissing <- floor(nrow(temp) * rate)
+  n <- nrow(data.complete)
+  r <- rank(data.complete %>% pull(pheno))
+  data.complete$int <- qnorm((r - k) / (n - 2 * k + 1))
+  data.missing$int <- NA
   
-  set.seed(seed)
-  missing_index <- sample(which(!is.na(temp$int)), size = nmissing)
-  
-  #oracle
-  temp$oracle <- temp$int
-  
-  # set phenotype missing value
-  temp[missing_index, ]$int <- NA
-  return(data.frame(temp %>% select(-paste0("f.", field, ".0.0"))))
+  return(rbind(data.complete, data.missing))
 }
+test <- INT(test, pheno_id) # INT of phenotype
+n <- nrow(test)
+r <- rank(test %>% pull(yhat))
+test$yhat_int <- qnorm((r - 0.375) / (n - 2 * 0.375 + 1)) #INT of yhat
 
-merge_data <- function(pdata, field = field_id, int = TRUE) {
-  rf_model <- readRDS(paste0("prediected_", field, ".rds"))
-  if (int) {
-    k <- 0.375
-    n <- nrow(rf_model)
-    r <- rank(rf_model$yhat)
-    rf_model$yhat <- qnorm((r - k) / (n - 2 * k + 1))
-  }
-  return(rf_model %>% inner_join(pdata, by = "f.eid"))
-}
-
-field_id <- 3148
-
-pdata <- create_missing_phenotypes(field = field_id, 
-                                   in_id_file = "in.id.rds", rate = 0)
-pheno <- merge_data(pdata = pdata)
+# Genetic Association Test
 G <- BEDMatrix::BEDMatrix(path = "final.bed", simple_names = T) # read genetic data
 
 cl <- makeCluster(type = "MPI")
 registerDoParallel(cl)
 
-clusterExport(cl, list("pheno"))
+clusterExport(cl, list("test","cov_id"))
 clusterEvalQ(cl, {
   library(dplyr)
   G <- BEDMatrix::BEDMatrix(path = "final.bed", simple_names = T) # read genetic data
 })
 
 results <- parLapply(cl, X = 1:ncol(G), fun = function(i) {
-  g <- as.numeric(G[as.character(pheno$f.eid), i]) # snp i
-  # observed phenotype ~ intercept + age + sex + 10 genetic PC + SNP_i
-  assoc.observed <- lm(int ~ ., data = data.frame(
-    cbind(pheno %>% select(int, f.21022.0.0, f.22001.0.0, starts_with("PC")), g)
-  ))
-  out <- summary(assoc.observed)$coefficients["g", c(1,2,4)] # only beta, se, p-val
+    g <- as.numeric(G[as.character(test$f.eid), i]) # snp i
+    # observed phenotype ~ intercept + age + sex + 10 genetic PC + SNP_i
+    assoc.observed <- lm(int ~ ., data = data.frame(
+      cbind(test %>% select(int, cov_id, starts_with("PC")), g)
+    ))
+    out <- summary(assoc.observed)$coefficients["g", c(1,2,4)] # only beta, se, p-val
   
-  # Bivariate model
-  g_complete <- g[!is.na(g)]
-  X.cov <- cbind(g_complete, (pheno %>% select(f.21022.0.0, f.22001.0.0, starts_with("PC")))[!is.na(g), ])
-  X.cov <- cbind(X.cov, rep(1, nrow(X.cov))) # append intercept
+    # Bivariate model
+    g_complete <- g[!is.na(g)]
+    X.cov <- cbind(g_complete, (test %>% select(cov_id,starts_with("PC")))[!is.na(g), ])
+    X.cov <- cbind(X.cov, rep(1, nrow(X.cov))) # append intercept
   
-  fit.binormal <- SurrogateRegression::Fit.BNLS(
-    t = pheno$int[!is.na(g)],
-    s = pheno$yhat[!is.na(g)],
-    X = X.cov
-  )
-  out <- c(out, fit.binormal@Regression.tab %>%
-             filter(Outcome == "Target" & Coefficient == "g_complete") %>%
-             select(Point, SE, p) %>% as.numeric())
+    fit.binormal <- SurrogateRegression::Fit.BNLS(
+      t = test$int[!is.na(g)],
+      s = test$yhat_int[!is.na(g)],
+      X = X.cov
+    )
+    out <- c(out, fit.binormal@Regression.tab %>%
+               filter(Outcome == "Target" & Coefficient == "g_complete") %>%
+               select(Point, SE, p) %>% as.numeric())
   
-  vas <- c("marginal","bi")
-  vis <- c("beta", "se", "p")
-  out <- c(colnames(G)[i], out)
-  names(out) <- c("rsid", as.vector(t(outer(vas, vis, paste, sep = "."))))
-  return(out)
+    vas <- c("marginal","bi")
+    vis <- c("beta", "se", "p")
+    out <- c(colnames(G)[i], out)
+    names(out) <- c("rsid", as.vector(t(outer(vas, vis, paste, sep = "."))))
+    return(out)
 })
 
 stopCluster(cl)
@@ -84,4 +103,4 @@ stopCluster(cl)
 results <- do.call(rbind, results)
 results <- data.frame(results)
 
-saveRDS(results, file = paste0("binormal_field=", field_id, "_analysis.rds"))
+saveRDS(results, file = paste0("pheno=", pheno_id, "_result.rds"))
--- Code/submitR.sh
+++ Code/submitR.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH --nodes=20
 #SBATCH --ntasks-per-node=40
-#SBATCH --time=0-23:00           # time (DD-HH:MM)
+#SBATCH --time=0-01:00           # time (DD-HH:MM)
 #module load NiaEnv/2018a
 module purge
 module load CCEnv
@@ -9,6 +9,11 @@ module load StdEnv/2020
 module load gcc/9.3.0 openmpi/4.0.3 r/4.0.2
 
 R_PROFILE=${HOME}/R/x86_64-pc-linux-gnu-library/4.0/snow/RMPISNOWprofile; export R_PROFILE
-mpirun -np 800 -bind-to core:overload-allowed R CMD BATCH --no-save ${name}.R
+mpirun -np 800 -bind-to core:overload-allowed R CMD BATCH --no-save "--args $pheno_id" ${name}.R
+${name}_pheno${pheno_id}.Rout
 
-# sbatch --export=name="mean_comparision" submit.sh
+sbatch --export=name="mean_comparision" submit.sh
+for pheno_id in f.23248.2.0
+do
+	sbatch --export=pheno_id=$pheno_id,name="data_analysis" submitR.sh -o result_${pheno_id}.out
+done
