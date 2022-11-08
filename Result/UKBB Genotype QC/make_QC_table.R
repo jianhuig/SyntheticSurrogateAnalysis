02c3706af1285d96899536aee19fbadf9c3f654f
jlgrons <79216661+jlgrons@users.noreply.github.com>
2022-11-08 09:47:05 -0500

    Update make_QC_table.R

--- Result/UKBB Genotype QC/make_QC_table.R
+++ Result/UKBB Genotype QC/make_QC_table.R
@@ -12,20 +12,20 @@ head(qc_data)
 colnames(qc_data) <- c("Chromosome",
                        "Initial Dataset",
                        "Variants with < 90% genotype rate",
-                       "Variants Remaining",
+                       "Variants left",
                        "Variants with HWE < 1e-5",
-                       "Variants Remaining",
+                       "Variants left",
                        "Variants with MAF < 5%", # Needs to be updated.
-                       "Variants Remaining",
+                       "Variants left",
                        "Variants with LD Pruning > 0.9",
-                       "Variants Remaining"
+                       "Variants left"
                        )
 
 # Make table for the manuscript.
 print(xtable(qc_data,
              align=c(
                "|p{2.5cm}|",
-               "|p{2.1cm}|",
+               "|p{2.15cm}|",
                rep("p{1.3cm}|", 9))
              ),
       include.rownames=FALSE)
