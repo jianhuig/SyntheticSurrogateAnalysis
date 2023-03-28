6aee290909801c0754ba1d30016d48316ea83edc
jianhuig <58570421+jianhuig@users.noreply.github.com>
2023-03-28 10:48:20 -0400

    Update submitR.sh

--- Code/submitR.sh
+++ Code/submitR.sh
@@ -6,13 +6,15 @@
 module purge
 module load CCEnv
 module load StdEnv/2020
-module load gcc/9.3.0 openmpi/4.0.3 r/4.0.2
+module load gcc/9.3.0 openmpi/4.0.3 r/4.2.2
 
-R_PROFILE=${HOME}/R/x86_64-pc-linux-gnu-library/4.0/snow/RMPISNOWprofile; export R_PROFILE
-mpirun -np 800 -bind-to core:overload-allowed R CMD BATCH --no-save "--args $pheno_id" ${name}.R
-${name}_pheno${pheno_id}.Rout
+#R_PROFILE=${HOME}/R/x86_64-pc-linux-gnu-library/4.0/snow/RMPISNOWprofile; export R_PROFILE
+# mpirun -np 800 -bind-to core:overload-allowed R CMD BATCH --no-save "--args $pheno_id" ${name}.R
+# ${name}_pheno${pheno_id}.Rout
+
+mpirun -np 800 R CMD BATCH SynSurr_imputation.R
 
 # for pheno_id in f.23248.2.0 f.23260.2.0 f.23265.2.0 f.23283.2.0 f.23277.2.0 f.23287.2.0
 # do
-# 	sbatch --export=pheno_id=$pheno_id,name="data_analysis" submitR.sh
+# 	sbatch --export=pheno_id=$pheno_id,name="SplitGWAS" submitR.sh
 # done
