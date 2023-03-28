d1ccf22f9e712f997b9b4259d0ff0da00f5608c8
jianhuig <58570421+jianhuig@users.noreply.github.com>
2023-03-28 10:48:11 -0400

    Update association.sh

--- Code/association.sh
+++ Code/association.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=40
-#SBATCH --time=0-01:00           # time (DD-HH:MM)
+#SBATCH --time=0-00:15           # time (DD-HH:MM)
 module purge
 module load CCEnv
 module load StdEnv/2020
 module load plink/2.00-10252019-avx2
 
-plink2 --bfile allchromosome --pheno height_permuted.txt --covar covariates.txt --covar-variance-standardize --linear
+plink2 --bfile allchromosome --pheno Data/height_imputed.txt --pheno-name int_oracle --covar Data/height_covariate.txt --covar-variance-standardize --glm hide-covar
