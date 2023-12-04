86275be97b81c42640425ccb3ce82370c90ec5bf
jianhuig <jianhui.gao@mail.utoronto.ca>
2023-12-04 14:56:44 -0500

    MNAR

---
+++ Code/Real Data Analysis/Height and FEV1/data_analysis.R
@@ -0,0 +1,113 @@
+library(data.table)
+library(dplyr)
+library(bigsnpr)
+library(doParallel)
+library(BEDMatrix)
+
+library(ranger)
+library(readr)
+library(dplyr)
+library(data.table)
+library(tidyr)
+
+#setwd("~/Desktop/SyntheticSurrogateAnalysis/Data")
+
+cov_id <- c("f.21022.0.0","f.22001.0.0","f.50.0.0","f.23098.0.0","f.23104.0.0") #age,sex,height, weight, BMI
+#pheno_id <- c("f.23248.2.0")
+pheno_id <- (commandArgs(TRUE))
+print(pheno_id)
+
+# split data into train and test
+file <- read.table("CombinedTotalMass.tab", header=TRUE, sep="\t")
+in_id <- fread("final.king.cutoff.in.id") %>% rename(f.eid = `#FID`) %>% select(-IID)
+out_id <- fread("final.king.cutoff.out.id") %>% rename(f.eid = `#FID`) %>% select(-IID)
+
+cov_column <- file %>% select(cov_id)
+
+# Filter UKB caucasian 
+white <- data.table::fread("Ancestry.tab")
+file <- file %>% inner_join(white) %>% filter(!is.na(f.22006.0.0))
+
+
+train <- file %>% inner_join(out_id) %>% tidyr::drop_na(pheno_id) %>% tidyr::drop_na(all_of(cov_id)) %>% select(all_of(pheno_id), all_of(cov_id))
+test <- file %>% inner_join(in_id) %>% tidyr::drop_na(all_of(cov_id))%>% select(f.eid, all_of(pheno_id), all_of(cov_id))
+
+# Random Forest model
+model.rf <- ranger::ranger(
+  data = train,
+  as.formula(paste0(pheno_id, "~."))
+)
+
+yhat <- predict(model.rf, data = test %>% select(-f.eid,-pheno_id))$predictions
+test$yhat <- yhat
+
+# Merge genetic PC
+pcs <- fread("UKB_PC.tab")[,1:11]
+colnames(pcs) <- c("f.eid", paste0("PC",1:10))
+test <- test %>% inner_join(pcs, by = "f.eid")
+
+# Inverse Normal Transformation
+INT <- function(data, pheno, k = 0.375){
+  missing_index <- which(is.na(data[[pheno]]))
+  data.complete <- data[-missing_index, ]
+  data.missing <-data[missing_index, ]
+  
+  n <- nrow(data.complete)
+  r <- rank(data.complete %>% pull(pheno))
+  data.complete$int <- qnorm((r - k) / (n - 2 * k + 1))
+  data.missing$int <- NA
+  
+  return(rbind(data.complete, data.missing))
+}
+test <- INT(test, pheno_id) # INT of phenotype
+n <- nrow(test)
+r <- rank(test %>% pull(yhat))
+test$yhat_int <- qnorm((r - 0.375) / (n - 2 * 0.375 + 1)) #INT of yhat
+
+# Genetic Association Test
+G <- BEDMatrix::BEDMatrix(path = "allchromosome.bed", simple_names = T) # read genetic data
+
+cl <- makeCluster(type = "MPI")
+registerDoParallel(cl)
+
+clusterExport(cl, list("test","cov_id"))
+clusterEvalQ(cl, {
+  library(dplyr)
+  G <- BEDMatrix::BEDMatrix(path = "allchromosome.bed", simple_names = T) # read genetic data
+})
+
+results <- parLapply(cl, X = 1:ncol(G), fun = function(i) {
+    g <- as.numeric(G[as.character(test$f.eid), i]) # snp i
+    # observed phenotype ~ intercept + age + sex + 10 genetic PC + SNP_i
+    assoc.observed <- lm(int ~ ., data = data.frame(
+      cbind(test %>% select(int, "f.21022.0.0","f.22001.0.0", starts_with("PC")), g)
+    ))
+    out <- summary(assoc.observed)$coefficients["g", c(1,2,4)] # only beta, se, p-val
+  
+    # Bivariate model
+    g_complete <- g[!is.na(g)]
+    X.cov <- cbind(g_complete, (test %>% select("f.21022.0.0","f.22001.0.0",starts_with("PC")))[!is.na(g), ])
+    X.cov <- cbind(X.cov, rep(1, nrow(X.cov))) # append intercept
+  
+    fit.binormal <- SurrogateRegression::Fit.BNLS(
+      t = test$int[!is.na(g)],
+      s = test$yhat_int[!is.na(g)],
+      X = X.cov
+    )
+    out <- c(out, fit.binormal@Regression.tab %>%
+               filter(Outcome == "Target" & Coefficient == "g_complete") %>%
+               select(Point, SE, p) %>% as.numeric())
+  
+    vas <- c("marginal","bi")
+    vis <- c("beta", "se", "p")
+    out <- c(colnames(G)[i], out)
+    names(out) <- c("rsid", as.vector(t(outer(vas, vis, paste, sep = "."))))
+    return(out)
+})
+
+stopCluster(cl)
+
+results <- do.call(rbind, results)
+results <- data.frame(results)
+
+saveRDS(results, file = paste0("pheno=", pheno_id, "_result.rds"))
---
+++ Code/Real Data Analysis/MNAR/MNAR_fev1.R
@@ -0,0 +1,136 @@
+library(dplyr)
+library(data.table)
+library(doParallel)
+
+#setwd("/Users/jianhuigao/Desktop/SyntheticSurrogateAnalysis/Data")
+
+# read height data
+dat <- readRDS('field_20150_cleaned.rds')
+
+# Filter UKB caucasian 
+white <- data.table::fread("Ancestry.tab")
+dat <- dat %>% inner_join(white) %>% filter(!is.na(f.22006.0.0))
+
+# remove NA heights
+dat <- dat %>% filter(!is.na(f.20150.0.0))
+# upper and lower 10%
+upper <- quantile(dat$f.20150.0.0, 0.9, na.rm = TRUE)
+lower <- quantile(dat$f.20150.0.0, 0.1, na.rm = TRUE)
+dat <- dat %>% mutate(ind = between(f.20150.0.0, lower, upper))
+
+# sample split
+train_id <- fread("final.king.cutoff.out.id") %>% rename(f.eid = FID) %>% select(-IID)
+test_id <- fread("final.king.cutoff.in.id") %>% rename(f.eid = FID) %>% select(-IID)
+
+train <- dat %>% inner_join(train_id)
+test <- dat %>% inner_join(test_id)  %>% mutate(age = f.21022.0.0, 
+                                                sex = f.22001.0.0,
+                                                waist = f.48.0.0, 
+                                                height = f.50.0.0,
+                                                hip = f.49.0.0,
+                                                smoking = f.20160.0.0,
+                                                bmi = f.21001.0.0,
+                                                basal = f.23105.0.0,
+                                                impedance = f.23106.0.0,
+                                                fev1 = f.20150.0.0) %>% select(f.eid, height, age, sex, waist, hip, smoking, bmi, basal, impedance, fev1, int, starts_with("PC"), ind) %>%
+  rename(oracle_int = int) %>%
+  na.omit()
+
+# Random Forest Model
+train <- train %>% mutate(age = f.21022.0.0, 
+                          sex = f.22001.0.0,
+                          waist = f.48.0.0, 
+                          height = f.50.0.0,
+                          hip = f.49.0.0,
+                          smoking = f.20160.0.0,
+                          bmi = f.21001.0.0,
+                          basal = f.23105.0.0,
+                          impedance = f.23106.0.0,
+                          fev1 = f.20150.0.0) %>%
+  filter(ind) %>%
+  select(fev1, age, sex, waist, height, smoking, bmi, basal, impedance) %>% 
+  na.omit() 
+
+model.rf <- ranger::ranger(
+  data = train,
+  fev1 ~ .
+)
+
+yhat <- predict(model.rf, 
+                data = test %>%
+                  select(age, sex, waist, height, smoking, bmi, basal, impedance))$predictions
+test$yhat <- yhat
+
+# Set NAs
+test$int <- test$fev1
+test$int[which(test$ind == FALSE)] <- NA
+
+# Inverse Normal Transformation
+k = 0.375
+data.complete <- test[which(test$ind == TRUE), ]
+n <- nrow(data.complete)
+r <- rank(data.complete %>% pull(int))
+test$int[which(test$ind == TRUE)] <- qnorm((r - k) / (n - 2 * k + 1))
+
+# Inverse Normal Transformation
+n <- nrow(test)
+r <- rank(test %>% pull(yhat))
+test$yhat_int <- qnorm((r - 0.375) / (n - 2 * 0.375 + 1)) #INT of yhat
+
+# Merge genetic PC
+#pcs <- fread("UKB_PC.tab")[,1:11]
+#colnames(pcs) <- c("f.eid", paste0("PC",1:10))
+#test <- test %>% inner_join(pcs, by = "f.eid")
+
+# Genetic Association Test
+G <- BEDMatrix::BEDMatrix(path = "allchromosome.bed", simple_names = T) # read genetic data
+
+cl <- makeCluster(type = "MPI")
+registerDoParallel(cl)
+
+clusterExport(cl, list("test"))
+clusterEvalQ(cl, {
+  library(dplyr)
+  G <- BEDMatrix::BEDMatrix(path = "allchromosome.bed", simple_names = T) # read genetic data
+})
+
+results <- parLapply(cl, X = 1:ncol(G), fun = function(i) {
+  g <- as.numeric(G[as.character(test$f.eid), i]) # snp i
+  # oracle
+  oracle <- lm(oracle_int ~ ., data = data.frame(
+    cbind(test %>% select(oracle_int, age, sex, starts_with("PC")), g)
+  ))
+  out <- summary(oracle)$coefficients["g", c(1,2,4)] # only beta, se, p-val
+  
+  # observed phenotype ~ intercept + age + sex + 10 genetic PC + SNP_i
+  assoc.observed <- lm(int ~ ., data = data.frame(
+    cbind(test %>% select(int, age, sex, starts_with("PC")), g)
+  ))
+  out <- c(out, summary(assoc.observed)$coefficients["g", c(1,2,4)]) # only beta, se, p-val
+  
+  # Bivariate model
+  g_complete <- g[!is.na(g)]
+  X.cov <- cbind(g_complete, (test %>% select(age, sex,starts_with("PC")))[!is.na(g), ])
+  X.cov <- cbind(X.cov, rep(1, nrow(X.cov))) # append intercept
+  
+  fit.binormal <- SurrogateRegression::FitBNR(
+    t = test$int[!is.na(g)],
+    s = test$yhat_int[!is.na(g)],
+    X = X.cov
+  )
+  out <- c(out, fit.binormal@Regression.tab %>%
+             filter(Outcome == "Target" & Coefficient == "g_complete") %>%
+             select(Point, SE, p) %>% as.numeric())
+  
+  vas <- c("oracle","marginal","bi")
+  vis <- c("beta", "se", "p")
+  out <- c(colnames(G)[i], out)
+  names(out) <- c("rsid", as.vector(t(outer(vas, vis, paste, sep = "."))))
+  return(out)
+})
+
+stopCluster(cl)
+results <- do.call(rbind, results)
+results <- data.frame(results)
+
+saveRDS(results, file = paste0("fev1_remove_top10bottom10.rds"))
