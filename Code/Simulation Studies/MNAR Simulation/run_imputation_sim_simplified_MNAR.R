e517f5000451edb4196b21ace225a888c2f6cc81
jlgrons <79216661+jlgrons@users.noreply.github.com>
2023-12-07 16:50:01 -0500

    Update run_imputation_sim_simplified_MNAR.R

--- Code/Simulation Studies/MNAR Simulation/run_imputation_sim_simplified_MNAR.R
+++ Code/Simulation Studies/MNAR Simulation/run_imputation_sim_simplified_MNAR.R
@@ -1,27 +1,28 @@
 # Balanced: removal of 10% from top and bottom.
+# setwd('~/Documents/GitHub/SyntheticSurrogateAnalysis')
 
 setting <- "balanced"
 ycut_u = 0.9
 ycut_l = 0.1
-source('Code/imputation_sim_simplified_MNAR.R')
+source('Code/Simulation Studies/MNAR Simulation/imputation_sim_simplified_MNAR.R')
 
 # Top: removal of 10% from top
 
 setting <- "top"
 ycut_u = 0.9
 ycut_l = 0 
-source('Code/imputation_sim_simplified_MNAR.R')
+source('Code/Simulation Studies/MNAR Simulation/imputation_sim_simplified_MNAR.R')
 
 # Bottom: removal of 10% from bottom
 
 setting <- "bottom"
 ycut_u = 1
 ycut_l = 0.1
-source('Code/imputation_sim_simplified_MNAR.R')
+source('Code/Simulation Studies/MNAR Simulation/imputation_sim_simplified_MNAR.R')
 
 # Unbalanced: removal of 10% from top and 5% from bottom
 
 setting <- "unbalanced"
 ycut_u = 0.9
 ycut_l = 0.05
-source('Code/imputation_sim_simplified_MNAR.R')
+source('Code/Simulation Studies/MNAR Simulation/imputation_sim_simplified_MNAR.R')
